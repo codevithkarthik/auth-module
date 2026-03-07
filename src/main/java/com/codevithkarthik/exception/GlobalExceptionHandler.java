621e8f774fb2b47b30c646e5b54ed7b0a36be267
Karthik <karthiktejakalluri@gmail.com>
2026-03-07 20:23:27 +0530

    Handling all exceptions in globalexceptionhandler

---
+++ src/main/java/com/codevithkarthik/exception/GlobalExceptionHandler.java
@@ -0,0 +1,76 @@
+package com.codevithkarthik.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.codevithkarthik.DTO.ResponseStructure;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	
+	@ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ResponseStructure<String>> handleBadCredentials(
+            BadCredentialsException ex){
+
+        ResponseStructure<String> rs = new ResponseStructure<>();
+
+        rs.setStatusCode(HttpStatus.UNAUTHORIZED.value());
+        rs.setMessage("Invalid Email or Password");
+        rs.setData(null);
+
+        return new ResponseEntity<>(rs,HttpStatus.UNAUTHORIZED);
+    }
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String,String>> handleValidation(
+	        MethodArgumentNotValidException ex){
+
+	    Map<String,String> errors = new HashMap<>();
+
+	    ex.getBindingResult().getFieldErrors().forEach(error ->
+	        errors.put(error.getField(), error.getDefaultMessage())
+	    );
+
+	    return new ResponseEntity<>(errors,HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(UsernameNotFoundException.class)
+	public ResponseEntity<String> handleUserNotFound(UsernameNotFoundException ex){
+	    return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(UserAlreadyExistsException.class)
+	public ResponseEntity<ResponseStructure<String>> handleUserExists(UserAlreadyExistsException ex){
+
+	    ResponseStructure<String> rs = new ResponseStructure<>();
+
+	    rs.setStatusCode(HttpStatus.CONFLICT.value());
+	    rs.setMessage(ex.getMessage());
+	    rs.setData(null);
+
+	    return new ResponseEntity<>(rs, HttpStatus.CONFLICT);
+	}
+	
+	//fallback exception handler.This prevents our API from returning ugly stack traces.
+	/*
+	 * 
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ResponseStructure<String>> handleGeneralException(Exception ex){
+
+	    ResponseStructure<String> rs = new ResponseStructure<>();
+
+	    rs.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+	    rs.setMessage("Something went wrong");
+	    rs.setData(null);
+
+	    return new ResponseEntity<>(rs,HttpStatus.INTERNAL_SERVER_ERROR);
+	}*/
+}
