b7f048038671b6d32f80ae7560ea3cc0983d47ce
Karthik <karthiktejakalluri@gmail.com>
2026-03-10 15:38:08 +0530

    Updated User Repositary

--- src/main/java/com/codevithkarthik/repositary/UserRepositary.java
+++ src/main/java/com/codevithkarthik/repositary/UserRepositary.java
@@ -4,11 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.codevithkarthik.entity.User;
 
+import jakarta.validation.constraints.Pattern;
+
 @Repository
 public interface UserRepositary extends JpaRepository<User, Long> {
 	
 	User findByEmail(String email);//Spring Automatically Creates Query
 
 	boolean existsByEmail(String email);
+
+	boolean existsByMobile(@Pattern(regexp = "^[6-9]\\d{9}$", message = "Invalid mobile number") String mobile);
 
 }
