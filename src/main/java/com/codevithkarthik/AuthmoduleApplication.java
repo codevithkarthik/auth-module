24905aa624506b23bc7404d4900abee39a8633ee
Karthik <karthiktejakalluri@gmail.com>
2026-03-10 16:05:15 +0530

    Added .env Dependency

--- src/main/java/com/codevithkarthik/AuthmoduleApplication.java
+++ src/main/java/com/codevithkarthik/AuthmoduleApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AuthmoduleApplication {
 	public static void main(String[] args) {
+	        
 		SpringApplication.run(AuthmoduleApplication.class, args);
 	}
 }
